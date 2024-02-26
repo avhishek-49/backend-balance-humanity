933765bc63640ad9adafd6028b59a7c768b405ea
abishek.paudel <abishek.paudel@swifttech.com.np>
2024-02-26 11:29:10 +0545

    commit

--- server/db/seeders/20240224035853-customer_account_info.js
+++ server/db/seeders/20240224035853-customer_account_info.js
@@ -8,22 +8,33 @@ module.exports = {
         account_number: '1234567890',
         mobile_number: '9862383579',
         citizenship_number: 'ABC123',
-        address: '123 Main St, Anytown, USA',
+        address: 'Baglung',
         email: 'Abishek@yopmail.com',
         created_at: new Date(),
         updated_at: new Date(),
-        customer_id:""
+        customer_id:"1"
       },
       {
-        amount: 7500.00,
+        amount: 7500000.00,
         account_number: '0987654321',
         mobile_number: '1234567890',
         citizenship_number: 'DEF456',
         address: '456 Elm St, Othertown, USA',
-        email: 'customer2@example.com',
+        email: 'gaurab@yopmail.com',
         created_at: new Date(),
         updated_at: new Date(),
-        customer_id:""
+        customer_id:"3"
+      },
+      {
+        amount: 500000.00,
+        account_number: '0987654321',
+        mobile_number: '1234567890',
+        citizenship_number: 'DEF456',
+        address: '456 Elm St, Othertown, USA',
+        email: 'himal@yopmail.com',
+        created_at: new Date(),
+        updated_at: new Date(),
+        customer_id:"2"
       },
       // Add more seed data as needed
     ], {});
